5c589fd799a6a4b34824678834d020348ec7dbd4
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-07-04 14:14:09 +0200

    Update components

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,4 +4,4 @@ const Cards = () => {
   return <h1>Cards</h1>;
 };
 
-export default Card;
+export default Cards;
---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Charts = () => {
+  return <h1>Cards</h1>;
+};
+
+export default Charts;
