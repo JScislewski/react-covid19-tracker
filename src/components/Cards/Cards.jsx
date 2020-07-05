1fcbdd568258ce9bde27a524cc80c4695e709994
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-07-05 23:36:09 +0200

    Add other cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import CountUp from "react-countup";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import styles from "./Cards.module.css";
 
-const Cards = (props) => {
-  console.log(props);
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return <CircularProgress color="primary" />;
+  }
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
@@ -12,9 +16,54 @@ const Cards = (props) => {
             <Typography color="textSecondary" gutterBottom>
               Infected
             </Typography>
-            <Typography variant="h5">REAL DATA</Typography>
+            <Typography variant="h5">
+              {" "}
+              <CountUp
+                start={0}
+                end={confirmed.value}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+          </CardContent>
+        </Grid>
+
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography variant="h5">
+              {" "}
+              <CountUp
+                start={0}
+                end={deaths.value}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
             <Typography color="textSecondary">REAL DATE</Typography>
-            <Typography variant="body2">Number of active cases</Typography>
+          </CardContent>
+        </Grid>
+
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Recovered
+            </Typography>
+            <Typography variant="h5">
+              {" "}
+              <CountUp
+                start={0}
+                end={recovered.value}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">REAL DATE</Typography>
           </CardContent>
         </Grid>
       </Grid>
